44a45531c74fdec4dc9bfcc0c2b8c363b6c0ea3b
Efendy27 <67112181+Efendy27@users.noreply.github.com>
2021-01-07 18:06:14 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta> charset="utf-8">
+<title>website kodlama test
+
+</head>
+<body>
+
+<button id="git">Sayfaya Git</button>
+
+<script>
+
+document.querySelector("#git").oneclick=function()
+{
+window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+}
+
+</script>
+</body>
+</html>
